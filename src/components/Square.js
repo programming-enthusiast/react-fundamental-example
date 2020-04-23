c10cc7c5d3dad804692f8db78a4cd221a055952c
programming enthusiast <miura.nakamoto@outlook.com>
2020-04-23 17:22:42 +0800

    Fix game board

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,15 +3,12 @@ import React from "react";
 export default class Square extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: null
-    };
   }
 
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
       </button>
     );
   }
