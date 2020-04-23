fcb4fe978ea63dc96d19ed4f385dbb883c1b1621
programming enthusiast <miura.nakamoto@outlook.com>
2020-04-23 16:46:04 +0800

    Add shopping list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import ShoppingList from "./components/ShoppingList";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ShoppingList name="Shopping List" />
     </div>
   );
 }
